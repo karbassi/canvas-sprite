1085a27d4cd7d1337cd8b6e13aeb3972d4bb630f
Ali Karbassi <ali@karbassi.com>
2020-04-15 15:51:35 -0500

    Directions

--- example/03-directions/index.js
+++ example/03-directions/index.js
@@ -2,10 +2,13 @@ const canvas = document.querySelector(".world");
 const ctx = canvas.getContext("2d");
 
 let guy;
+let sprites = [];
 let walkingRight;
 let walkingLeft;
 let walkingUp;
 let walkingDown;
+
+const moveSpeed = 5;
 
 function init() {
   walkingRight = new Sprite({
@@ -32,6 +35,8 @@ function init() {
     frameCount: 9,
   });
 
+  sprites.push(walkingRight, walkingLeft, walkingUp, walkingDown);
+
   guy = walkingRight;
 
   tick();
@@ -46,22 +51,31 @@ function tick() {
   window.requestAnimationFrame(tick);
 }
 
+function updateLocation(x, y) {
+  sprites.forEach(function (sprite) {
+    sprite.x = x;
+    sprite.y = y;
+  });
+}
+
 function onKeyDown(event) {
   const key = event.key.toLowerCase();
 
   if (key === "a" || key === "arrowleft") {
     guy = walkingLeft;
-    guy.x -= 1;
+    guy.x -= moveSpeed;
   } else if (key === "d" || key === "arrowright") {
     guy = walkingRight;
-    guy.x += 1;
+    guy.x += moveSpeed;
   } else if (key === "w" || key === "arrowup") {
     guy = walkingUp;
-    guy.y -= 1;
+    guy.y -= moveSpeed;
   } else if (key === "s" || key === "arrowdown") {
     guy = walkingDown;
-    guy.y += 1;
+    guy.y += moveSpeed;
   }
+
+  updateLocation(guy.x, guy.y);
 }
 
 document.addEventListener("keydown", onKeyDown);
--- example/README.md
+++ example/README.md
@@ -7,3 +7,7 @@ A standalone example
 ## [With AnimationFrame](./02-with-animationframe)
 
 An example with [AnimationFrame](https://github.com/karbassi/Canvas-Animation-Frame).
+
+## [With Multiple Sprites](./03-directions)
+
+Multiple sprites to showcase movement.
